34d1c76513845f6d677d74c58162e016ccb02fb4
Bagwan Shahabaz <>
2022-10-07 09:06:13 +0200

    Add openCV support

--- example/wxImage/src/image.cpp
+++ example/wxImage/src/image.cpp
@@ -9,7 +9,6 @@
 // Licence:     wxWindows licence
 ///////////////////////////////////////////////////////////////////////////////
 
-// #include "canvas.h"
 #include <project-lucid/lib.h>
 
 // ============================================================================
@@ -874,6 +873,35 @@ void MyFrame::OnAbout( wxCommandEvent& WXUNUSED( event ) )
                       wxICON_INFORMATION | wxOK );
 }
 
+wxImage wx_from_mat( cv::Mat& img )
+{
+  using namespace cv;
+  Mat im2;
+  if( img.channels() == 1 ) {
+    cvtColor( img, im2, cv::COLOR_GRAY2RGB );
+  } else if( img.channels() == 4 ) {
+    cvtColor( img, im2, COLOR_BGRA2RGB );
+  } else {
+    cvtColor( img, im2, COLOR_BGR2RGB );
+  }
+  long imsize = im2.rows * im2.cols * im2.channels();
+  wxImage wx( im2.cols, im2.rows, (unsigned char*)malloc( imsize ), false );
+  unsigned char* s = im2.data;
+  unsigned char* d = wx.GetData();
+  for( long i = 0; i < imsize; i++ ) {
+    d[ i ] = s[ i ];
+  }
+  return wx;
+}
+
+cv::Mat mat_from_wx( wxImage& wx )
+{
+  using namespace cv;
+  Mat im2( Size( wx.GetWidth(), wx.GetHeight() ), CV_8UC3, wx.GetData() );
+  cvtColor( im2, im2, COLOR_RGB2BGR );
+  return im2;
+}
+
 wxString MyFrame::LoadUserImage( wxImage& image )
 {
   wxString filename;
@@ -881,6 +909,7 @@ wxString MyFrame::LoadUserImage( wxImage& image )
 #if wxUSE_FILEDLG
   filename = wxLoadFileSelector( "image", wxEmptyString );
   if( !filename.empty() ) {
+
     if( !image.LoadFile( filename ) ) {
       wxLogError( "Couldn't load image from '%s'.", filename );
 
@@ -896,6 +925,7 @@ void MyFrame::OnNewFrame( wxCommandEvent& WXUNUSED( event ) )
 {
   wxImage image;
   wxString filename = LoadUserImage( image );
+
   if( !filename.empty() )
     new MyImageFrame( this, filename, image );
 }
--- include/project-lucid/lib.h
+++ include/project-lucid/lib.h
@@ -8,13 +8,12 @@
 #include <wx/mstream.h>
 #include <wx/quantize.h>
 #include <wx/scopedptr.h>
+#include <wx/scrolwin.h>
 #include <wx/spinctrl.h>
 #include <wx/stopwatch.h>
 #include <wx/versioninfo.h>
 #include <wx/wfstream.h>
 #include <wx/wx.h>
-
-#include <wx/scrolwin.h>
 
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
