d54a6bf8380d535d9eeb5aeb12a202b0b8269269
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2023-07-16 19:00:28 +0200

    update example and main projct as per IMGUI

--- include/project-lucid/lib.h
+++ include/project-lucid/lib.h
@@ -1,19 +1,14 @@
 #pragma once
 
-#include <wx/artprov.h>
-#include <wx/file.h>
-#include <wx/filename.h>
-#include <wx/graphics.h>
-#include <wx/image.h>
-#include <wx/mstream.h>
-#include <wx/quantize.h>
-#include <wx/scopedptr.h>
-#include <wx/scrolwin.h>
-#include <wx/spinctrl.h>
-#include <wx/stopwatch.h>
-#include <wx/versioninfo.h>
-#include <wx/wfstream.h>
-#include <wx/wx.h>
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+#include <imgui/imgui.h>
+#include <stdio.h>
+#define GL_SILENCE_DEPRECATION
+#if defined( IMGUI_IMPL_OPENGL_ES2 )
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
